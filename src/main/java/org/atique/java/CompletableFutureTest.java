4d53056fc698da797c48fd2e297e68ef2a60bbec
atiQue <atiqurrahman7465@gmail.com>
2024-04-21 15:26:14 +0600

    [java-concurrency] CompletableFutureTest.java

---
+++ src/main/java/org/atique/java/CompletableFutureTest.java
@@ -0,0 +1,44 @@
+package org.atique.java;
+
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author atiQue
+ * @since 21'Apr 2024 at 12:57 PM
+ */
+
+public class CompletableFutureTest {
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor pool = (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter the task time: ");
+            long taskTime = sc.nextLong();
+            if (taskTime == 0) {
+                System.out.println("Killing the process ....");
+                break;
+            }
+
+            CompletableFuture.supplyAsync(() -> {
+                        try {
+                            Thread.sleep(taskTime);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return taskTime;
+                    }, pool)
+                    .thenAccept((result) -> System.out.println("Task: " + result + " finished"));
+
+            System.out.println("Task: " + taskTime + " submitted!");
+        }
+
+
+        pool.shutdown();
+        System.out.println("Process killed ...");
+    }
+}
