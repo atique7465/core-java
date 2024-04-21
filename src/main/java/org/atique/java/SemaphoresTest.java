4a339ce45b9709bf7f16d6600b7522230bb94594
atiQue <atiqurrahman7465@gmail.com>
2024-04-21 15:46:25 +0600

    [java-concurrency] SemaphoresTest.java

---
+++ src/main/java/org/atique/java/SemaphoresTest.java
@@ -0,0 +1,40 @@
+package org.atique.java;
+
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author atiQue
+ * @since 21'Apr 2024 at 9:43 AM
+ */
+
+public class SemaphoresTest {
+
+    private static Scanner sc = new Scanner(System.in);
+    private static int value = 100;
+
+    private static Semaphore semaphore = new Semaphore(3, true);
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Enter the task time: ");
+            long taskTime = sc.nextLong();
+
+            if(taskTime == 0) break;
+
+            Runnable runnable = () -> {
+                try {
+                    semaphore.acquire();
+                    Thread.sleep(taskTime);
+                    System.out.println("Tasktime: " + taskTime + " accessed value: " + value);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+
+            new Thread(runnable).start();
+        }
+
+    }
+}
