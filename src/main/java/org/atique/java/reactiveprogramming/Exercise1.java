4d954a369c27c5673077fdf683059d00a93af209
atiQue <atiqurrahman7465@gmail.com>
2024-05-16 12:48:52 +0600

    [core-java] ReactiveProgramming Exercise1 implementation.

---
+++ src/main/java/org/atique/java/reactiveprogramming/Exercise1.java
@@ -0,0 +1,43 @@
+package org.atique.java.reactiveprogramming;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+        // Use StreamSources.intNumbersStream() and StreamSources.userStream()
+
+        // Print all numbers in the intNumbersStream stream
+        StreamSources.intNumbersStream().forEach(System.out::println);
+
+        // Print numbers from intNumbersStream that are less than 5
+        StreamSources.intNumbersStream().filter(e -> e < 5).forEach(System.out::println);
+
+        // Print the second and third numbers in intNumbersStream that's greater than 5
+        StreamSources.intNumbersStream()
+                .filter(e -> e > 5)
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
+
+        //  Print the first number in intNumbersStream that's greater than 5.
+        //  If nothing is found, print -1
+        Integer res = StreamSources.intNumbersStream()
+                .filter(e -> e > 5)
+                .findFirst()
+                .orElse(-1);
+        System.out.println(res);
+
+        // Print first names of all users in userStream
+        StreamSources.userStream().forEach(e -> System.out.println(e.getFirstName()));
+
+        // Print first names in userStream for users that have IDs from number stream
+        StreamSources.userStream()
+                .filter(u -> StreamSources.intNumbersStream().anyMatch(i -> i == u.getId()))
+                .forEach(u -> System.out.println(u.getFirstName()));
+
+        StreamSources.intNumbersStream()
+                .flatMap(id -> StreamSources.userStream().filter(u -> u.getId() == id))
+                .forEach(u -> System.out.println(u.getFirstName()));
+    }
+
+}
